52fc0631a9ad801959c1b8d8775b273857ffbdbe
KrzysKond <gaxkk19@gmail.com>
2024-09-01 10:46:40 +0200

    fixed linting

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,12 @@
 Database models
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users"""
@@ -23,7 +24,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None, **extra_field):
         """Create and return superuser"""
-        user =  self.create_user(email, password, **extra_field)
+        user = self.create_user(email, password, **extra_field)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -34,7 +35,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
