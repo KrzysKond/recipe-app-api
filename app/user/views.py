9830e29101a9c86499f3422b07db3de2ea324720
KrzysKond <gaxkk19@gmail.com>
2024-09-02 10:29:45 +0200

    added partially user apis

---
+++ app/user/views.py
@@ -0,0 +1,32 @@
+"""
+Views for the user API
+"""
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+    )
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateUserTokenView(ObtainAuthToken):
+    """Create a new token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveAPIView):
+    """Manage the authenicated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthenitcation]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """retrieve and return the authenticated user"""
+        return self.request.user
