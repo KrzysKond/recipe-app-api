d2daa3122dbf54cd639e103cd87e869572eaa883
KrzysKond <gaxkk19@gmail.com>
2024-09-03 15:33:50 +0200

    added recipe serializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+"""
+Serializers for recipe APIs
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+class RecipeSerializers(serializers.ModelSerializer):
+    """Serializers for the recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
