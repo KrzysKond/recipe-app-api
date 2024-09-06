dd3ac75ddbc4950e97c7b1a1137882814e03e001
KrzysKond <gaxkk19@gmail.com>
2024-09-06 19:30:03 +0200

    fixed typos

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,7 @@
 set -e
 
 python manage.py wait_for_db
-python manage.py collecstatic --noinput
+python manage.py collectstatic --noinput
 python manage.py migrate
+
+uwisgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
